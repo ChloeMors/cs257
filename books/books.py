454a79bad5a5fffec968dfaa06353a71f9f7754f
XinyanXiang <52022947+XinyanXiang@users.noreply.github.com>
2021-10-09 16:05:08 -0500

    revise 'none' and list1

--- books/books.py
+++ books/books.py
@@ -14,9 +14,9 @@ search in getName and get fullname for author search
 revise main
 revise and use __str__ and __repr__ functions
 hardcoded filename
-list1 in main - Xinyan
+
 string formatting  - Xinyan
-dont use "none" as default  - Xinyan
+
 redo usage
 display functions: I wrote a display authors 2 - we might want to rethink where these functions go
  - should they be functions in the author class?
@@ -26,6 +26,8 @@ snake case - chloe
 double quotes - chloe
 probably can delete lines 65-68 in booksdatasource
 added str and repr functinos but Im not sure theyre right, and I didnt use them anywhere
+list1 in main - Xinyan
+dont use "empty" as default  - Xinyan
 '''
 from booksdatasource import Author, Book, BooksDataSource
 
@@ -45,9 +47,9 @@ class bcolors:
 
 def get_parsed_arguments():
     parser = argparse.ArgumentParser(add_help=False, description="Search and sort books and authors.")
-    parser.add_argument("-t", "--title", const = "None", nargs="?") 
-    parser.add_argument("-a", "--author", nargs="?", const="None") 
-    parser.add_argument("-y", "--year", nargs="?", const="None") 
+    parser.add_argument("-t", "--title", const = "empty", nargs="?") 
+    parser.add_argument("-a", "--author", nargs="?", const="empty") 
+    parser.add_argument("-y", "--year", nargs="?", const="empty") 
     parser.add_argument("-s", nargs=1, default="title", choices=["year", "title"])
     parser.add_argument("-h", "--help", action = "store_true", dest="hi")
     parser.add_argument("-v", "--version", action="version", version = "%(prog)s 1.0, Chloe Morscheck and Xinyan Xiang, CS 257, Oct 2, 2021")
@@ -64,40 +66,40 @@ def main():
         print(file_contents)
         f.close
     books_data_source = BooksDataSource("books1.csv")  
-    list1 = []
+    books_list = []
 
     if arguments.title:
-        if arguments.title == "None":
+        if arguments.title == "empty":
             print(bcolors.WARNING + "Note: Since you did not specify any strings for books, here are all books in this database." + bcolors.ENDC)
-            list1 = books_data_source.books(sort_by=arguments.s[0])
-            books_data_source.display_books(list1)
+            books_list = books_data_source.books(sort_by=arguments.s[0])
+            books_data_source.display_books(books_list)
         else:
-            list1 = books_data_source.books(search_text=arguments.title, sort_by=arguments.s[0])
-            books_data_source.display_books(list1)
+            books_list = books_data_source.books(search_text=arguments.title, sort_by=arguments.s[0])
+            books_data_source.display_books(books_list)
     if arguments.author:
-        if arguments.author == "None":
+        if arguments.author == "empty":
             print(bcolors.WARNING + "Note: Since you did not specify any strings for authors , here are all authors in this database." + bcolors.ENDC)
-            list1 = books_data_source.authors()
-            books_data_source.display_authors(list1)
+            books_list = books_data_source.authors()
+            books_data_source.display_authors(books_list)
         else: 
-            list1 = books_data_source.authors(search_text=arguments.author)
-            books_data_source.display_authors(list1)
+            books_list = books_data_source.authors(search_text=arguments.author)
+            books_data_source.display_authors(books_list)
     if arguments.year:
-        if arguments.year == "None":
+        if arguments.year == "empty":
             print(bcolors.WARNING + "Note: Since you did not specify any years for authors , here are all authors in this database." +  bcolors.ENDC)
-            list1 = books_data_source.books_between_years()
+            books_list = books_data_source.books_between_years()
         elif "-" in arguments.year:
             year_list = arguments.year.split(" ")
             if len(year_list) == 3:             
-                list1 = books_data_source.books_between_years(start_year = year_list[0], end_year = year_list[2])
+                books_list = books_data_source.books_between_years(start_year = year_list[0], end_year = year_list[2])
             else:
                 if year_list[0].isdigit(): 
-                    list1 = books_data_source.books_between_years(start_year = year_list[0])
+                    books_list = books_data_source.books_between_years(start_year = year_list[0])
                 else:
-                    list1 = books_data_source.books_between_years(end_year = year_list[1])
+                    books_list = books_data_source.books_between_years(end_year = year_list[1])
         else: 
-            list1 = books_data_source.books_between_years(start_year = arguments.year, end_year = arguments.year)        
-        books_data_source.display_books(list1)
+            books_list = books_data_source.books_between_years(start_year = arguments.year, end_year = arguments.year)        
+        books_data_source.display_books(books_list)
 
 
 
